e8b0a449bb7a1cf055c198520b684b941382bf8f
Cogithub2016 <judas.liscariote.007@gmail.com>
2018-06-28 16:34:38 +0200

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express =  require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getCurrentYear', () => {return new Date().getFullYear();});
@@ -56,6 +57,6 @@ app.get('/',(req,res) => {
   app.get('/bad', (req,res) => {
     res.send({errorMessage: 'Something went wrong!'});
   })
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
